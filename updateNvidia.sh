2e49bad2a5c1a5c0aa098632748d3e99aad03493
ComasSky <jacquot.hadrien@gmail.com>
2019-09-16 16:16:29 +0200

    Reverse default choice

--- updateNvidia.sh
+++ updateNvidia.sh
@@ -23,11 +23,11 @@ echo ""
 
 if [ "${array[0]}" != "$INSTALLED_DRIVER" ] && [  "${array[0]}" != "" ]
 then
- read -p "Do you wish to update Nvidia driver ($INSTALLED_DRIVER => ${array[0]}) ? Y/n : " yn
+ read -p "Do you wish to update Nvidia driver ($INSTALLED_DRIVER => ${array[0]}) ? y/N : " yn
   case $yn in
-        [Nn]* ) exit;;
-            * ) wget  --no-check-certificate -P "$FOLDER" "https://international.download.nvidia.com/XFree86/Linux-x86_64/${array[1]}"; chmod +x "$FOLDER/$NAME";;
-  esac
+        [yY]* ) wget  --no-check-certificate -P "$FOLDER" "https://international.download.nvidia.com/XFree86/Linux-x86_64/${array[1]}"; chmod +x "$FOLDER/$NAME";;
+	 * ) exit;; 
+ esac
 fi
 else
 echo ""
